5716c6b3376573041772827b527e33cd4f6f1acb
Muhammad Motasim <70141975@student.uol.edu.pk>
2026-02-23 02:55:14 +0500

    update save tab button

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if(leadfromlocalStorage){
 
 tabBtn.addEventListener("click",function(){
     chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-        myLeads.push(tabs)
+        myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads",JSON.stringify(myLeads))
         render(myLeads)
     })
